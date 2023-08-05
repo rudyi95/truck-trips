6670d1498e9d847c2cefd9fc4f63da6f18c52b3c
rudyi95 <rud11@meta.ua>
2023-08-05 23:24:17 +0200

    fixes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { MainPage, AddTripPage, TripsPage } from "./pages";
+import { Layout } from "./containers";
 
 const Routers: React.FC = () => {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="add-trip" element={<AddTripPage />} />
-        <Route path="trips" element={<TripsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="add-trip" element={<AddTripPage />} />
+          <Route path="trips" element={<TripsPage />} />
+        </Route>
         <Route path="*" element={<div style={{ padding: 20 }}>not found</div>} />
       </Routes>
     </div>
