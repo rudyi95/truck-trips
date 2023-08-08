fe4e8ee2012f869603a12f88fabb50fa8d753464
rudyi95 <rud11@meta.ua>
2023-08-09 00:09:40 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import Routes from "./Routes";
 
 import "./App.css";
 
-const router = createBrowserRouter([{ path: "*", Component: Routes }]);
+const router = createHashRouter([{ path: "*", Component: Routes }]);
 
 const App: React.FC = () => {
   return <RouterProvider router={router} />;
